057ab1d7cd1161e98e1f11e2d858c1c0602db77c
Victor Johan Palma Rodriguez <vicjohan23@gmail.com>
2022-11-07 16:09:44 -0400

    Se creo un nuevo controlador para consumir del api de casa michel

---
+++ DataAccess/Models/Producto.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    public class Producto
+    {
+        public int sec { get; set; }
+        public string codigo { get; set; }
+        public string codBar { get; set; }
+        public string cod_Fab { get; set; }
+        public object comentario { get; set; }
+        public string descripcion { get; set; }
+        public string und { get; set; }
+        public int empaque { get; set; }
+        public string detalle { get; set; }
+        public bool itbis { get; set; }
+        public double porITBIS { get; set; }
+        public int precio { get; set; }
+        public int precioOferta { get; set; }
+        public double costo { get; set; }
+        public int precioLista { get; set; }
+        public int xMayor1 { get; set; }
+        public double xMayor2 { get; set; }
+        public double xMayor3 { get; set; }
+        public double precioMinimo { get; set; }
+        public int enOferta { get; set; }
+        public int areaID { get; set; }
+        public int dptoID { get; set; }
+        public int grupoID { get; set; }
+        public string area { get; set; }
+        public string dpto { get; set; }
+        public object grupo { get; set; }
+        public string foto { get; set; }
+        public int inventario { get; set; }
+        public bool pesado { get; set; }
+        public int addCantidad { get; set; }
+        public int quantity { get; set; }
+        public int agregado { get; set; }
+        public string descripcionLarga { get; set; }
+        public string codigoJuntos { get; set; }
+        public string codigoJuntos1 { get; set; }
+        public List<string> fotosAlterna { get; set; }
+        public object productosLink { get; set; }
+        public int link { get; set; }
+    }
+}
---
+++ chitecapi/Controllers/Consulta_AzureController.cs
@@ -0,0 +1,99 @@
+﻿using chitecapi.Responses;
+using DataAccess;
+using DataAccess.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace chitecapi.Controllers
+{
+    public class Consulta_AzureController : ApiController
+    {
+
+        /// <summary>
+        /// Obtine un producto
+        /// </summary>
+        /// <param name="codigo" example="7460103900141">Codigo</param>
+        /// <param name="sucursal" example="1">Sucursal</param>
+        /// <param name="Nivel" example="4">Nivel</param>
+        /// <param name="Factor" example="1">Factor</param>
+        /// <remarks>
+        /// Se utiliza para obtener un producto, generado con un formato JSON:
+        /// 
+        ///     Example Result:
+        ///     {
+        ///         "producto": 
+        ///                   {
+        ///                      "sec": 10774,
+        ///                      "codigo": "7460103900141",
+        ///                      "codBar": "N10774",
+        ///                      "cod_Fab": "INV.PELO",
+        ///                      "comentario": null,
+        ///                      "descripcion": "INVITACIONES CUMPLEAÑOS 10PCS OSIRIS ESPECIALES",
+        ///                      "und": "UND",
+        ///                      "empaque": 1,
+        ///                      "detalle": "",
+        ///                      "itbis": true,
+        ///                      "porITBIS": 0.152542372881356,
+        ///                      "precio": 60,
+        ///                      "precioOferta": 60,
+        ///                      "costo": 31.388,
+        ///                      "xMayor1": 58,
+        ///                      "xMayor2": 55.1,
+        ///                      "xMayor3": 52.2,
+        ///                      "precioMinimo": 49.3,
+        ///                      "enOferta": 0,
+        ///                      "areaID": 39,
+        ///                      "dptoID": 23,
+        ///                      "grupoID": 0,
+        ///                      "area": "FIESTAS Y CUMPLEAÑOS",
+        ///                      "dpto": "FIESTAS TEMATICAS",
+        ///                      "grupo": null,
+        ///                      "foto": "https://www.casamichelrd.com/imagenes\\23\\7460103900141.jpg",
+        ///                      "inventario": 40,
+        ///                      "pesado": false,
+        ///                      "addCantidad": 1,
+        ///                      "quantity": 1,
+        ///                      "agregado": 0,
+        ///                      "descripcionLarga": "",
+        ///                      "codigoJuntos": "0",
+        ///                      "codigoJuntos1": "0",
+        ///                      "fotosAlterna": [
+        ///                       "https://www.casamichelrd.com/imagenes\\23\\7460103900141.jpg"
+        ///                      ],
+        ///                      "productosLink": null,
+        ///                      "link": 0
+        ///                    }
+        ///                 
+        ///     }
+        /// </remarks>
+        [HttpGet]
+        public async Task<IHttpActionResult> Get(Int64 codigo, int sucursal = 1, int Nivel = 4, int Factor = 1)
+        {
+            string endpoint = ConfigurationManager.AppSettings["consultaazure"];
+            endpoint = endpoint
+                .Replace("{sucursal}", sucursal.ToString())
+                .Replace("{nivel}", Nivel.ToString())
+                .Replace("{factor}", Factor.ToString());
+
+            var producto = new Producto();
+
+            HttpClient http = new HttpClient();
+
+            string data = await http.GetStringAsync(endpoint + codigo);
+
+            producto = JsonConvert.DeserializeObject<Producto>(data);
+
+            return Json(producto);
+        }
+    }
+}
+
+
+
